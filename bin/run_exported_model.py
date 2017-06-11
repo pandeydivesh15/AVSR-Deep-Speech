03331fec86321e3bf4018faabc12ae569951adc6
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2017-06-11 13:23:22 +0530

    Adds code for running a trained Deep Speech model(exported after training). The model takes input as wav files.

---
+++ bin/run_exported_model.py
@@ -0,0 +1,39 @@
+import argparse
+import glob
+import sys
+import os
+# Make sure that we can import functions/classes from utils/ folder
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from util.exported_model import DeepSpeechModel
+
+parser = argparse.ArgumentParser(
+	description='Run a trained Speech to Text model for some inputs. \
+	**NOTE: Please use a trailing slash for directory names.')
+
+parser.add_argument('-d', '--export_dir', type=str,
+					help="Dir where the trained model's meta graph and data were exported")
+parser.add_argument('-wd', '--wav_dir', type=str,
+					help='Dir where wav files are stored (files for conversion)')
+parser.add_argument('-n', '--model_name', type=str,
+					help='Name of the model exported')
+
+args = parser.parse_args()
+
+export_dir = args.export_dir or 'data/export/00000001/'
+model_name = args.model_name or 'export'
+
+model = DeepSpeechModel(export_dir, model_name)
+
+model.restore_model()
+
+if args.wav_dir:
+	wav_file_paths = sorted(glob.glob(args.wav_dir + '*.wav'))
+else:
+	wav_file_paths = ['data/ldc93s1/LDC93S1.wav']
+
+transcripts = model.find_transcripts(wav_file_paths)
+print transcripts
+
+# Close session
+model.close()
---
+++ util/exported_model.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import numpy as np
+
+from util.text import ndarray_to_text
+from util.audio import audiofile_to_input_vector
+
+# These constants must be same as those used during training.
+NUM_MFCC_COEFF = 26
+N_CONTEXT = 9
+
+class DeepSpeechModel(object):
+	"""Handles with trained Deep Speech model"""
+	def __init__(self, export_dir, model_name):
+		'''
+		Args:
+			export_dir(type = str): Path to directory where trained model 
+									has been exported (with trailing slash).
+			model_name(type = str): Name of the model exported.
+		'''
+		self.export_dir = export_dir
+		self.session = tf.Session()
+		self.name = model_name
+
+	def restore_model(self):
+		# Load meta graph and learned weights
+		saver = tf.train.import_meta_graph(self.export_dir + self.name + '.meta')
+		saver.restore(self.session, tf.train.latest_checkpoint(self.export_dir))
+
+		graph = tf.get_default_graph()
+		self.input = graph.get_tensor_by_name("input_node:0")
+		self.input_len = graph.get_tensor_by_name("input_lengths:0")
+		self.output = graph.get_tensor_by_name("output_node:0")
+
+	def find_transcripts(self, wav_file_paths):
+		'''
+		Args: 
+			wav_file_paths: A list containing filepaths for each wav file.
+							Type of each element = str 
+		'''
+		transcripts = []
+
+		# TODO: Currently, session.run() runs multiple times(once for each wav file). 
+		# This is due to different batch sizes for each file.
+		# Find a way to run the model only once.
+		# Make batch size equal for all, and predict for entire batch at once.
+
+		for path in wav_file_paths:
+			source = np.array([(audiofile_to_input_vector(path, NUM_MFCC_COEFF , N_CONTEXT))])
+			source_len = np.array([(len(source[-1]))])
+
+			feed_dict = {self.input:source, self.input_len:source_len}
+
+			batch_decoded = self.session.run(self.output, feed_dict)
+			for decoded in batch_decoded[0]:
+				transcripts.append(ndarray_to_text(decoded))
+
+		return transcripts
+
+	def close(self):
+		self.session.close()
+
+
